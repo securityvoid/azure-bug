d518f5546b260479cee6d429d6d32e403f94d815
Douglas Logan <dlogan@cyberninjas.com>
2017-02-21 16:47:59 -0500

    Remove requires

--- azure.deps.js
+++
@@ -1,4 +0,0 @@
-	global.azureDeps = (global.azureDeps) ? global.azureDeps : {};
-	global.azureDeps["q"] =	require("q");
-	global.azureDeps["path"] =	require("path");
-	global.azureDeps["xml2js"] =	require("xml2js");
