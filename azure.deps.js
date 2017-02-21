151b5c9af47fd00a09b861970f3e4473d27a02cb
Douglas Logan <dlogan@cyberninjas.com>
2017-02-21 13:08:04 -0500

    Initial Commit

---
+++ azure.deps.js
@@ -0,0 +1,4 @@
+	global.azureDeps = (global.azureDeps) ? global.azureDeps : {};
+	global.azureDeps["q"] =	require("q");
+	global.azureDeps["path"] =	require("path");
+	global.azureDeps["xml2js"] =	require("xml2js");
